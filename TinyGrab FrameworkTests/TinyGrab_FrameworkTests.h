1248e702ccd51fdc1cc06ff0a6c88b48c69a7593
Sam Gray <isamgray@me.com>
2011-03-18 05:26:32 -0500

    Initial Commit

---
+++ TinyGrab FrameworkTests/TinyGrab_FrameworkTests.h
@@ -0,0 +1,17 @@
+//
+//  TinyGrab_FrameworkTests.h
+//  TinyGrab FrameworkTests
+//
+//  Created by Sam Gray on 3/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface TinyGrab_FrameworkTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ TinyGrab FrameworkTests/TinyGrab_FrameworkTests.m
@@ -0,0 +1,33 @@
+//
+//  TinyGrab_FrameworkTests.m
+//  TinyGrab FrameworkTests
+//
+//  Created by Sam Gray on 3/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "TinyGrab_FrameworkTests.h"
+
+
+@implementation TinyGrab_FrameworkTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TinyGrab FrameworkTests");
+}
+
+@end
